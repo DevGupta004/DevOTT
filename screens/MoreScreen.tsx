a2f3ab3b6539b3b4c05eb3a047376d38e05244f2
Dev Gupta <dev.gupta@mogiio.com>
2022-09-07 04:04:14 +0530

    Home Screen

--- screens/MoreScreen.tsx
+++ screens/MoreScreen.tsx
@@ -7,8 +7,8 @@ export default function MoreScreen() {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>More</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/MoreScreen.tsx" />
+      {/* <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+      <EditScreenInfo path="/screens/MoreScreen.tsx" /> */}
     </View>
   );
 }
--- screens/MylistScreen.tsx
+++ screens/MylistScreen.tsx
@@ -7,8 +7,8 @@ export default function MylistScreen() {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>MyList</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/MylistScreen.tsx" />
+      {/* <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+      <EditScreenInfo path="/screens/MylistScreen.tsx" /> */}
     </View>
   );
 }
